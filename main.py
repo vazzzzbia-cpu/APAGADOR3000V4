8bc61d70cb91625b115ae7a553925352b80965b4
vazzzzbia-cpu <vazzzzbia@gmail.com>
2026-07-06 18:43:51 -0300

    Adicionar Flask ao Render

--- main.py
+++ main.py
@@ -37,6 +37,19 @@ def executar():
         drop_pending_updates=True
     )
 
+from flask import Flask
+import threading
+import os
 
+web = Flask(__name__)
+
+@web.route("/")
+def home():
+    return "Bot online!"
+
+def servidor():
+    web.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
+threading.Thread(target=servidor, daemon=True).start()
 if __name__ == "__main__":
     iniciar_bot(executar)
